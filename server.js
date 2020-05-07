4c767450b99abb2f2f815e804ad93d272ee66cb4
ch04937 <97hernandez.c@gmail.com>
2020-05-07 14:48:29 -0500

    feat: create server for chess game

---
+++ server.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+
+const port = process.env.PORT || 4000;
+
+const chessRoute = require("./routes/chessRoute");
+
+const server = express();
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.use("/chess", chessRoute);
+
+server.listen(port, () => {
+	console.log(`\n *** Server listening on port ${port} ***\n`);
+});
